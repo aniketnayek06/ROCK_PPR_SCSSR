af07d8cb61c3a3eea4174a781afb9f5b129ca700
Aniket Nayek <aniketnayek575@gmail.com>
2024-10-27 12:47:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playtheGAme(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
+
+  if (playerChoice === computerChoice) {
+    result = "IT'S A TIE";
+  } else {
+    switch (playerChoice) {
+      case "rock":
+        result = computerChoice === "scissors" ? "YOU WIN" : "YOU LOSE";
+        break;
+      case "paper":
+        result = computerChoice === "rock" ? "YOU WIN" : "YOU LOSE";
+        break;
+      case "scissors":
+        result = computerChoice === "paper" ? "YOU WIN" : "YOU LOSE";
+        break;
+    }
+  }
+
+  playerDisplay.textContent = `PLAYER"S CHOICE : ${playerChoice}`;
+  computerDisplay.textContent = `COMPUTER'S CHOICE : ${computerChoice}`;
+  resultDisplay.textContent = result;
+
+  switch (result) {
+    case "YOU WIN":
+      playerScore++;
+      playerScoreDisplay.textContent = playerScore;
+      break;
+    case "YOU LOSE":
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
+      break;
+  }
+}
